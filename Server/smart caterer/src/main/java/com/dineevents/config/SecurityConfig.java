8657f354c91627ec1b97358c79ed0d90f5ae45de
shamos13 <akwatuha04@gmail.com>
2026-07-16 16:29:05 +0300

    Bypass 401 by disabling CSRF

---
+++ Server/smart caterer/src/main/java/com/dineevents/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.dineevents.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+//Temporal Bypass to 401
+@Configuration
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
