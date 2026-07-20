7a9ff727e55dface0bd554680c17f8f70160e85d
shamos13 <akwatuha04@gmail.com>
2026-07-20 18:07:39 +0300

    Staff module

---
+++ Server/smart caterer/src/main/java/com/dineevents/staff/contorller/StaffContorller.java
@@ -0,0 +1,25 @@
+package com.dineevents.staff.contorller;
+
+
+import com.dineevents.staff.DTO.Request.StaffRequestDTO;
+import com.dineevents.staff.DTO.Response.StaffResponseDTO;
+import com.dineevents.staff.StaffRepository;
+import com.dineevents.staff.StaffService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/staff")
+public class StaffContorller {
+
+    private final StaffService staffService;
+
+    @PostMapping("/new-staff")
+    public ResponseEntity<StaffResponseDTO> createStaff(StaffRequestDTO staffRequestDTO) {
+        return  ResponseEntity.ok(staffService.createStaff(staffRequestDTO));
+    }
+}
