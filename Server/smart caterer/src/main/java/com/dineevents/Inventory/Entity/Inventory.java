8985bdc3903a4471f261b6a806bf0e766b845be4
shamos13 <akwatuha04@gmail.com>
2026-07-22 13:50:06 +0300

    Refactoring the Inventor

--- Server/smart caterer/src/main/java/com/dineevents/Inventory/Entity/Inventory.java
+++ Server/smart caterer/src/main/java/com/dineevents/Inventory/Entity/Inventory.java
@@ -9,6 +9,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Entity
 @Getter
 @Setter
@@ -21,6 +23,5 @@ public class Inventory {
     private Long inventoryId;
     private String inventoryName;
     private int inventoryQuantity;
-    private double inventoryPrice;
 
 }
