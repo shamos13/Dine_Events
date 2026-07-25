6794d1849a401dc4ce1423a135f8906635237503
shamos13 <akwatuha04@gmail.com>
2026-07-25 22:26:06 +0300

    Quotation module

---
+++ Server/smart caterer/src/main/java/com/dineevents/Quotation/Entity/Quotation.java
@@ -0,0 +1,56 @@
+package com.dineevents.Quotation.Entity;
+
+import com.dineevents.Quotation.Enum.QuotationStatus;
+import com.dineevents.event.Entity.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Quotation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long quotationId;
+    private String quotationNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    @Column(nullable = false)
+    private BigDecimal subTotal;
+
+    @Column(nullable = false)
+    private BigDecimal total;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private QuotationStatus quotationStatus;
+
+    private LocalDate validUntil;
+
+    @Column(nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    //Relationship with Quotation Line items will come here (One Quotation will be linked to many Quotation items)
+    @OneToMany(mappedBy = "quotation", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<QuotationLineItem> lineItems;
+
+
+    @PrePersist
+    private void onCreate(){
+        this.createdAt = OffsetDateTime.now();
+    }
+}
---
+++ Server/smart caterer/src/main/java/com/dineevents/Quotation/Enum/LineItemType.java
@@ -0,0 +1,8 @@
+package com.dineevents.Quotation.Enum;
+
+public enum LineItemType {
+    MENU_PACKAGE,
+    RENTAL,
+    SERVICE,
+    CUSTOM
+}
