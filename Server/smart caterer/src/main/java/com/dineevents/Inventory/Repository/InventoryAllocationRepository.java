3aef3e44e61eea747e5712021e488f7c10aa59a6
shamos13 <akwatuha04@gmail.com>
2026-07-26 01:01:06 +0300

    quotation module 2.o

--- Server/smart caterer/src/main/java/com/dineevents/Inventory/Repository/InventoryAllocationRepository.java
+++ Server/smart caterer/src/main/java/com/dineevents/Inventory/Repository/InventoryAllocationRepository.java
@@ -1,9 +1,14 @@
 package com.dineevents.Inventory.Repository;
 
 import com.dineevents.Inventory.Entity.InventoryItemAllocation;
+import com.dineevents.event.Entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InventoryAllocationRepository extends JpaRepository<InventoryItemAllocation, Long> {
+
+    List<InventoryItemAllocation> findByEvent(Event event);
 }
