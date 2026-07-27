86f371f51b1fb10eac1410297e0b4ac030414e3e
shamos13 <akwatuha04@gmail.com>
2026-07-27 16:40:11 +0300

    Invoice Generation: Added Invoice Entity,

---
+++ Server/smart caterer/src/main/java/com/dineevents/Invoice/DTO/Response/InvoiceResponseDTO.java
@@ -0,0 +1,24 @@
+package com.dineevents.Invoice.DTO.Response;
+
+
+import com.dineevents.Invoice.Enum.InvoiceStatus;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+
+@Data
+public class InvoiceResponseDTO {
+        private Long invoiceId;
+        private String invoiceNumber;
+        private String eventName;
+        private String clientName;
+        private BigDecimal amountDue;
+        private BigDecimal amountPaid;
+        private BigDecimal balance;
+        private LocalDate dueDate;
+        private InvoiceStatus invoiceStatus;
+        private OffsetDateTime createdAt;
+
+}
---
+++ Server/smart caterer/src/main/java/com/dineevents/Invoice/Repository/InvoiceRepository.java
@@ -0,0 +1,9 @@
+package com.dineevents.Invoice.Repository;
+
+import com.dineevents.Invoice.Entity.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InvoiceRepository extends JpaRepository<Invoice,Long> {
+}
