cea4d352fa52f097ec7d31ed24da582f4e2c0a75
shamos13 <akwatuha04@gmail.com>
2026-07-29 02:28:15 +0300

    Authentication and authorization

---
+++ Server/smart caterer/src/main/java/com/dineevents/auth/DTO/Request/LoginRequestDTO.java
@@ -0,0 +1,12 @@
+package com.dineevents.auth.DTO.Request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequestDTO {
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+}
