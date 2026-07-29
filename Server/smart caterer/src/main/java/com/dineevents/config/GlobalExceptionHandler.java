7595c15f25f10e32075b80ecc9d8830a2f1336cd
shamos13 <akwatuha04@gmail.com>
2026-07-29 22:32:22 +0300

    Auth layer: Added exception handling

--- Server/smart caterer/src/main/java/com/dineevents/config/GlobalExceptionHandler.java
+++ Server/smart caterer/src/main/java/com/dineevents/config/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.dineevents.config;
 
 import jakarta.persistence.EntityNotFoundException;
+import com.dineevents.auth.Exception.InvalidRefreshTokenException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -46,6 +47,14 @@ public class GlobalExceptionHandler {
             HttpServletRequest request
     ) {
         return build(HttpStatus.BAD_REQUEST, exception.getMessage(), request, null);
+    }
+
+    @ExceptionHandler(InvalidRefreshTokenException.class)
+    public ResponseEntity<ApiErrorResponse> handleInvalidRefreshToken(
+            InvalidRefreshTokenException exception,
+            HttpServletRequest request
+    ) {
+        return build(HttpStatus.UNAUTHORIZED, exception.getMessage(), request, null);
     }
 
     @ExceptionHandler(IllegalStateException.class)
