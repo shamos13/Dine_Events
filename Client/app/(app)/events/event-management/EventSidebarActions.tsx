230e0cdfacbf1acb85ca1c4d307eea888a271107
shamos13 <akwatuha04@gmail.com>
2026-07-29 22:33:46 +0300

    Fixed pricing subtotal calculation

--- Client/app/(app)/events/event-management/EventSidebarActions.tsx
+++ Client/app/(app)/events/event-management/EventSidebarActions.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { ClipboardList, FileText, ReceiptText } from 'lucide-react'
 import { OutlineButton } from './components'
 import { EventTotals } from './EventTotals'
+import type { LineItemType } from '@/lib/api/quotations'
 
 interface EventSidebarActionsProps {
   eventId: number
@@ -11,6 +12,7 @@ interface EventSidebarActionsProps {
   onGenerateProposal?: () => void
   onCreateReport?: () => void
   className?: string
+  liveTotals?: Partial<Record<LineItemType, number>>
 }
 
 export default function EventSidebarActions({
@@ -19,10 +21,11 @@ export default function EventSidebarActions({
   onGenerateProposal,
   onCreateReport,
   className = '',
+  liveTotals,
 }: EventSidebarActionsProps) {
   return (
     <aside className={`space-y-6 ${className}`}>
-      <EventTotals eventId={eventId} />
+      <EventTotals eventId={eventId} liveTotals={liveTotals} />
       <div className="space-y-3">
         <OutlineButton onClick={onCreateReport} className="w-full">
           <FileText className="h-4 w-4" />
